79e64cbb60e6ca103c9bddc080d5f14655ea7eca
Adam Sparks <adamsparks92@gmail.com>
2026-06-09 13:57:41 -0700

    add canvas integration for sentry

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -7,7 +7,11 @@ Sentry.init({
   // To disable sending user data, uncomment the line below. For more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/solidstart/configuration/options/#dataCollection
   // dataCollection: { userInfo: false },
-  integrations: [solidRouterBrowserTracingIntegration(), Sentry.replayIntegration()],
+  integrations: [
+    solidRouterBrowserTracingIntegration(),
+    Sentry.replayIntegration(),
+    Sentry.replayCanvasIntegration(),
+  ],
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Session Replay
